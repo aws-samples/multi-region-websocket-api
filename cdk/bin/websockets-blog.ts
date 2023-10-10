29825e034f0bd20ddba10812bcac07c1cec4bcba
Ben Freiberg <9841563+bfreiberg@users.noreply.github.com>
2023-10-10 14:14:38 +0200

    Add description and metric id to Stack

--- cdk/bin/websockets-blog.ts
+++ cdk/bin/websockets-blog.ts
@@ -13,6 +13,7 @@ regionsToDeploy.forEach((regionCode) => {
   const stack = new WebsocketsBlogStack(app, `WebsocketsBlogStack-${regionCode}`, {
     env: { region: regionCode },
     regionCodesToReplicate: regionsToDeploy.filter((replicationRegion) => replicationRegion !== regionCode),
+    description: 'Deploys the regional Stack for multi-region Websocket API demo (uksb-1tupboc29)',
   });
   Tags.of(stack).add('project', 'aws-blogpost');
   Tags.of(stack).add('topic', 'multi-region-websocket-api');
